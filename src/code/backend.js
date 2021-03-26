1ba2fd08e38f8637e72a082a53c5f67f5219f0d1
OliverKovacs <oliver.kovacs.dev@gmail.com>
2021-03-26 23:52:20 +0100

    Minor fixes

--- src/code/backend.js
+++ src/code/backend.js
@@ -49,7 +49,7 @@ const backend = new class {
     async onload () {
 
         const display = new Engine.Canvas("main");
-        const scene = new Engine.Scene();
+        const scene = [];
         const engine = new Engine(scene, [ display ]);
 
         await engine.addEntity(new Engine.Entity({
